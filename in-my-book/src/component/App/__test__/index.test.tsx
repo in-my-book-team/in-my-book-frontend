98af358b725fac404573a264a190141cec4fe2e4
Olya <polyanskaya3000@gmail.com>
2023-01-31 14:32:49 +0700

    test: finish 'run test no watch' mode during pre-push

--- in-my-book/src/component/App/__test__/index.test.tsx
+++ in-my-book/src/component/App/__test__/index.test.tsx
@@ -4,6 +4,6 @@ import App from '../App';
 
 it('renders welcome message', () => {
     render(<App />);
-    expect(screen.getByText('Hello')).toBeInTheDocument();
-});
 
+    expect(screen.getByText('Start Project')).toBeInTheDocument();
+});
