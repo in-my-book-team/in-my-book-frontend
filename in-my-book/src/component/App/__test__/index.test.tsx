0b4b84421ad10a183e4814ed156bd880dd53e98e
Olya <polyanskaya3000@gmail.com>
2023-01-31 14:30:10 +0700

    test: test 'run test no watch' mode during pre-push

--- in-my-book/src/component/App/__test__/index.test.tsx
+++ in-my-book/src/component/App/__test__/index.test.tsx
@@ -1,7 +1,9 @@
 import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
 import App from '../App';
 
 it('renders welcome message', () => {
     render(<App />);
-    expect(screen.getByText('Start Project')).toBeInTheDocument();
+    expect(screen.getByText('Hello')).toBeInTheDocument();
 });
+
